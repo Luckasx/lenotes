71dc596d5cd33e0d66d9ccff4d58689b1cbbde8d
luckasx <luckasx@gmail.com>
2022-07-01 17:40:37 -0300

    added roboto font and set visibility select

--- client/src/components/note-form.jsx
+++ client/src/components/note-form.jsx
@@ -1,16 +1,23 @@
 import React from "react";
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
+import { Row, Col, Form, Button } from "react-bootstrap";
 
 export default class NoteForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       text: "",
+      visibility: 0
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.saveNote = this.saveNote.bind(this);
+    this.handleVisibility = this.handleVisibility.bind(this);
+  }
+
+  handleVisibility(event){
+    this.setState({
+      visibility: event.target.value,
+    });
   }
 
   handleChange(event) {
@@ -33,18 +40,39 @@ export default class NoteForm extends React.Component {
 
   render() {
     return (
-      <Form>
-        <Form.Label>Insert New Note</Form.Label>
-        <Form.Control
-          as="textarea"
-          rows={3}
-          value={this.state.text}
-          onChange={this.handleChange}
-        />
-        <Button className="mt-2" variant="primary" onClick={this.saveNote}>
-          Save
-        </Button>
-      </Form>
+      <Row>
+        <Col sm="12" md="4" >
+          <Form>
+            <Row>
+              <Col>
+                <Form.Label>Type a New Note</Form.Label>
+                <Form.Control
+                  as="textarea"
+                  rows={3}
+                  value={this.state.text}
+                  onChange={this.handleChange}
+                />
+              </Col>
+            </Row>
+            <Row className="mt-1 align-items-center ">
+              <Col sm="4" md="3">
+                <Form.Label>Choose visibility:</Form.Label>
+              </Col>
+              <Col sm="8" md="6">
+                <Form.Select onChange="handleVisibility">
+                  <option selected value="0">
+                    Private
+                  </option>
+                  <option value="1">Public</option>
+                </Form.Select>
+              </Col>
+            </Row>
+            <Button className="mt-2" variant="primary" onClick={this.saveNote}>
+              Save
+            </Button>
+          </Form>
+        </Col>
+      </Row>
     );
   }
 }
