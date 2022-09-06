ab369e381cbf033b661361d3543ed1b5520eb138
luckasx <luckasx@gmail.com>
2022-09-06 08:10:45 -0300

    Implement Signup Responsive Form Layout

--- client/src/views/Signup.jsx
+++ client/src/views/Signup.jsx
@@ -5,20 +5,31 @@ import LeNavBar from "../components/LeNavBar";
 import { Row, Col, Form, Button } from "react-bootstrap";
 
 export default class Signup extends Component {
+
+  constructor() {
+    super();
+    this.state = { checkboxChecked: false };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+
+  handleChange(evt) {
+    this.setState({ checkboxChecked: evt.target.checked });
+  }
+
   render() {
     return (
       <div className="App container-fluid">
-        <Row>
+        <Row className="nav-bar-mb">
           <LeNavBar />
         </Row>
         <Row className="form-signup">
-          <Col sm={{span: 4, offset:4}}>
+          <Col lg={{span: 2, offset:5}} md={{span: 6, offset:3}} sm={{span: 4, offset:4}}>
             <Form>
               <Form.Group className="mb-3" controlId="formBasicUsername">
                 <Form.Label>Username</Form.Label>
                 <Form.Control type="text" placeholder="Choose your username" />
-                <Form.Text className="text-muted">
-                  We'll never share your email with anyone else.
+                <Form.Text className="text-muted">                  
                 </Form.Text>
               </Form.Group>
               <Form.Group className="mb-3" controlId="formBasicEmail">
@@ -32,13 +43,19 @@ export default class Signup extends Component {
                 <Form.Label>Password</Form.Label>
                 <Form.Control type="password" placeholder="Password" />
               </Form.Group>
-              <Form.Group className="mb-3" controlId="formBasicCheckbox">
+              <Form.Group className="mb-3" controlId="formBasicPasswordRepeat">
+                <Form.Label>Repeat Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+              </Form.Group>
+              <Form.Group className="mb-3" >
                 <Form.Check
+                  controlId="formBasicCheckbox"
                   type="checkbox"
                   label="I agree with everything you want"
+                  onChange={this.handleChange}
                 />
               </Form.Group>
-              <Button variant="primary" type="submit">
+              <Button variant="primary" type="submit" disabled={!this.state.checkboxChecked}>
                 Submit
               </Button>
             </Form>
