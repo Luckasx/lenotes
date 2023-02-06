3d6beda3bea060a14455ac6e6769a1fb013df476
Lucas Viana <luckasx@gmail.com>
2023-02-06 07:55:26 -0300

    Handle Login Submit

--- client/src/components/SignInForm.jsx
+++ client/src/components/SignInForm.jsx
@@ -2,6 +2,7 @@ import { React, useState } from "react";
 import { useForm } from "react-hook-form";
 import { Form, Button } from "react-bootstrap";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export default function SignUpForm() {
   const {
@@ -10,33 +11,36 @@ export default function SignUpForm() {
     handleSubmit,
   } = useForm();
 
-  const [userRegistered, setRegistered] = useState(false);
-  const [errorOnSignin, setErrorOnSignin] = useState(false);
+  let navigate = useNavigate(); // declare here, inside a React component.
 
-  
-  const onSubmit = (data) => {
-    console.log(data);
-    axios.post("api/user/login", { data }).then((res) => {
-      if (res.data.msg === "User Created.") {
-        setRegistered(true);
+  const [errorOnSignin, setErrorOnSignin] = useState(false);
+  const [invalidLogin, setInvalidLogin] = useState(false);
+
+  /**
+   * 
+   * @param {*} data Form data with username/password
+   * @returns Shows error or redirect to home
+   */
+  const onSubmit = async (data) => {
+    try {
+      //clear previous error messages
+      setInvalidLogin(false);
+      setErrorOnSignin(false);
+
+      let res = (await axios.post("api/user/login", { data })).data;
+
+      if (res.username) {
+        navigate("/");
+      }
+    } catch (err) {
+      //when the response status is 401. we assume it is an invalid login
+      if (err.response.status === 401) {
+        setInvalidLogin(true);
         return;
       }
-
       setErrorOnSignin(true);
-      
-    });
+    }
   };
-
-  if (userRegistered) {
-    return (
-      <div>
-        <h3>User created with success!</h3>
-        <Button variant="link" href="signin">
-          Sign In
-        </Button>
-      </div>
-    );
-  }
 
   return (
     <div>
@@ -44,7 +48,7 @@ export default function SignUpForm() {
         <Form.Group className="mb-3" controlid="formBasicUsername">
           <Form.Control
             {...register("username", {
-              required: "An username is required",             
+              required: "An username is required",
             })}
             type="text"
             autoComplete="off"
@@ -56,28 +60,32 @@ export default function SignUpForm() {
 
           <Form.Text className="text-muted"></Form.Text>
         </Form.Group>
-  
+
         <Form.Group className="mb-3" controlid="formBasicPassword">
           <Form.Control
             type="password"
             placeholder="Password"
             {...register("password", {
-              required: "A password is required",             
+              required: "A password is required",
             })}
           />
-          
         </Form.Group>
-    
-        
-        <Button variant="primary" type="submit" >
+
+        <Button variant="primary" type="submit">
           Sign in
         </Button>
       </Form>
       {errorOnSignin && (
-        <h2>
+        <h5>
           There is some problem with your request. Please try again in a few
           minutes.
-        </h2>
+        </h5>
+      )}
+
+      {invalidLogin && (
+        <h5 class='text-danger'>
+          Invalid user or password.
+        </h5>
       )}
     </div>
   );
